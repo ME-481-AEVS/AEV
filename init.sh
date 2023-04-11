e356509862f1f4b6cf6493f756f8bdb3367b2550
Owen Bramley <35117462+owenbramley@users.noreply.github.com>
2023-04-10 15:37:38 -1000

    added init script & livestream test

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install git
+sudo apt-get install python
+git clone https://github.com/ME-481-AEVS/AEV.git
+pip install -r AEV/requirements.txt
+python main.py
