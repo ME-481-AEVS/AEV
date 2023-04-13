81408e24a628dba8a0cd2dcf4df1a03fe6e11930
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-04-12 20:59:41 -1000

    Update streaming code for jetson

--- webstreaming.py
+++ webstreaming.py
@@ -5,23 +5,22 @@ import random
 import time
 import cv2
 
-# initialize a flask object
-app = Flask(__name__)
+
 # initialize the video stream and allow the camera sensor to start up
-camera_0 = cv2.VideoCapture(0)  # have to double-check which camera is which
-camera_1 = cv2.VideoCapture(1)
-camera_2 = cv2.VideoCapture(2)
-time.sleep(2.0)
+# camera_0 = cv2.VideoCapture(0)  # have to double-check which camera is which
 
 
-@app.route("/")
-def index():
-    # return the rendered template
-    return render_template("index.html")
+def __gstreamer_pipeline(camera_id, capture_width=1280, capture_height=720, display_width=1280, display_height=720, framerate=30, flip_method=0):
+    return (
+            "nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=True"
+            % (camera_id, capture_width, capture_height, framerate, flip_method, display_width, display_height)
+    )
 
 
 def generate(camera):
-    # loop over frames from the output stream
+    """ Loop over frames from the output stream """
     while True:
         success, frame = camera.read()  # read the camera frame
         if not success:
@@ -34,25 +33,47 @@ def generate(camera):
                    frame + b'\r\n')
 
 
+# initialize a flask object
+app = Flask(__name__)
+
+stream = cv2.VideoCapture(0)
+
+time.sleep(2.0)  # give camera time to start up
+
+print('cam has image : %s' % stream.read()[0])  # True = image captured, False = :(
+
+if not stream.read():
+    stream.open()
+if not stream.isOpened():
+    print('Cannot open camera')
+
+
+@app.route("/")
+def index():
+    # return the rendered template
+    return render_template("index.html")
+
+
 @app.route("/camera0")
 def camera0():
     # return the response generated along with the specific media
     # type (mime type)
-    return Response(generate(camera_0), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(generate(stream), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
+"""
 @app.route("/camera1")
 def camera1():
     # return the response generated along with the specific media
     # type (mime type)
     return Response(generate(camera_1), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-
 @app.route("/camera2")
 def camera2():
     # return the response generated along with the specific media
     # type (mime type)
     return Response(generate(camera_2), mimetype="multipart/x-mixed-replace; boundary=frame")
+"""
 
 
 @app.get("/telemetry")
@@ -64,4 +85,3 @@ def telemetry():
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
