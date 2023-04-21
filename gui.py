185e529acc83f5a4f362206a192c2a31fe43fc74
robertgodfrey <robertgodfrey@outlook.com>
2023-04-21 00:01:37 -1000

    Add actuator controls and basic GUI

---
+++ gui.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+# Create an instance of tkinter window
+win = Tk()
+
+# Define the geometry of the window
+win.geometry('1024x600')
+
+frame = Frame(win, width=1024, height=600)
+frame.pack()
+frame.place(anchor='center', relx=0.5, rely=0.5)
+
+img = Image.open('/media/images/logo-tri.png')
+resized_image = img.resize((600,600), Image.ANTIALIAS)
+new_image = ImageTk.PhotoImage(resized_image)
+
+# Create a Label Widget to display the text or Image
+label = Label(frame, image = new_image)
+label.pack()
+
+win.mainloop()
+
