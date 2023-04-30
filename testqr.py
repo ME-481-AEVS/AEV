3feed759f5e9cbda85b3078dea6a7ee9faeee15e
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-04-29 16:31:23 -1000

    Add basic human detection

--- testqr.py
+++ testqr.py
@@ -1,6 +1,9 @@
 from flask import Response
 from flask import Flask
 from flask import render_template
+from imutils.object_detection import non_max_suppression
+import imutils
+import numpy as np
 import threading
 import time
 import cv2
@@ -16,6 +19,8 @@ scan_qr_code = True
 time.sleep(2.0)
 
 stream_active = False
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 
 def qr_code_loop():
@@ -39,24 +44,37 @@ def index():
 
 
 def generate():
+    global stream_active
     # loop over frames from the output stream
     while True:
-        time.sleep(0.1)
         success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
+            image = imutils.resize(frame, width=min(600, frame.shape[1]))
+            orig = image.copy()
+            (rects, _) = hog.detectMultiScale(image, winStride=(6, 6), padding=(10, 10), scale=1.15)
+
+            for (x, y, w, h) in rects:
+                cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+            pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+            for (xA, yA, xB, yB) in pick:
+                cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
             if scan_qr_code:
-                data, bbox, _ = detector.detectAndDecode(frame)
+                data, _, _ = detector.detectAndDecode(image)
                 if data:
                     print(data)
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+
+            _, buffer = cv2.imencode('.jpg', image)
+            image = buffer.tobytes()
 
             stream_active = False
             # yield the output frame in the byte format - hangs here if not actively viewed!!
             yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
-                   frame + b'\r\n')
+                   image + b'\r\n')
             stream_active = True
 
 
