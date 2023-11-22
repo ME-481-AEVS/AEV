395046a30dd36644dbc8def579e51cc92e02974c
Rob Godfrey <rob_godfrey@outlook.com>
2023-11-22 04:45:58 -1000

    Add ssl gunicorn cmd

--- flask_app/start.sh
+++ flask_app/start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sudo gunicorn --bind 0.0.0.0:80 --workers 3 --timeout 0 run:app
+# sudo gunicorn --bind 0.0.0.0:80 --threads 3 --timeout 0 run:app
+sudo gunicorn --bind 0.0.0.0:443 --threads 3 --certfile ssl/server.pem --keyfile ssl/server.key --timeout 0 run:app
