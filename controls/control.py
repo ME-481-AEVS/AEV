96458fd3c2be89f2613a65f63e628d2164fc2c6f
owenbramley <owen.bramley03@gmail.com>
2024-03-11 14:59:26 -1000

    added new controls

---
+++ controls/control.py
@@ -0,0 +1,9 @@
+# TEAM WAEVS
+# Date: 02/20/2021
+# Purpose: this program will format commands to be sent to the Arduino
+# Authors: Owen Bramley
+
+from extra.communication import *
+
+# testing the sendCommand function from communication.py
+sendCommand("<T[>") # get temperature test
---
+++ controls/extra/communication.py
@@ -0,0 +1,32 @@
+# TEAM WAEVS
+# Date: 02/20/2021
+# Purpose: This program is used to communicate with the Arduino using the serial port.
+# The program sends a command to the Arduino and then waits for a response.
+# Authors: Owen Bramley 
+
+import serial
+import time
+
+# open communication to the serial port of the Arduino 
+BAUDRATE = 115200
+PORT = '/dev/cu.usbmodem101'
+try:
+    serialcomm = serial.Serial(PORT, BAUDRATE)
+    serialcomm.timeout = 1
+except:
+    print("Could not communicate with arduino. Check the port and baudrate.")
+    exit() # exit the program if the serial port cannot be opened
+
+def sendCommand(command = '0'):
+    i = command.strip()
+    if i == '0':
+        return "Invalid command"
+    serialcomm.write(i.encode())
+    time.sleep(0.5)
+    # read the response from the Arduino
+    print(serialcomm.readline().decode('ascii'))
+    serialcomm.close() # close the serial port
+
+# log data recived from the Arduino
+# catch any errors that may occur and add them to the log
+
