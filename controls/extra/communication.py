a777b3f27d0184131451e8f1bb26f064b089f0cf
k0m0code <komochristian@gmail.com>
2024-03-15 12:30:26 -1000

    modified:   controls/extra/communication.py

--- controls/extra/communication.py
+++ controls/extra/communication.py
@@ -19,6 +19,9 @@ except:
     print("Could not communicate with arduino. Check the port and baudrate.")
     exit() # exit the program if the serial port cannot be opened
 
+#send command to arduino and request response
+#first parameter: takes command in "<>" form
+#second parameter: log data on/off
 def sendCommand(command = '0', log = False):
     i = command.strip()
     if i == '0':
@@ -26,16 +29,12 @@ def sendCommand(command = '0', log = False):
     serialcomm.write(i.encode())
     time.sleep(0.5)
     # read the response from the Arduino
-    if (Log):
+    try:
+        if (Log): # log data recived from the Arduino if log is True
+            file = open(LOGFILE, "a")
+            file.write(serialcomm.readline().decode('ascii'))
+        print(serialcomm.readline().decode('ascii'))
+        serialcomm.close() # close the serial port
+    except:
         file = open(LOGFILE, "a")
-        file.write(serialcomm.readline().decode('ascii'))
-    print(serialcomm.readline().decode('ascii'))
-    serialcomm.close()
-    return "Complete" # close the serial port
-
-
-# log data recived from the Arduino
-
-
-# catch any errors that may occur and add them to the log
-
+        file.write("Error") # caught errors added to log
