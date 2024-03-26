328b655de6a755388adb6f9ce4b6248bda12ee26
owenbramley <owen.bramley03@gmail.com>
2024-03-25 14:51:53 -1000

    update

--- controls/extra/communication.py
+++ controls/extra/communication.py
@@ -1,15 +1,17 @@
 # TEAM WAEVS
-# Date: 02/20/2021
+# Date: 03/20/2024
 # Purpose: This program is used to communicate with the Arduino using the serial port.
 # The program sends a command to the Arduino and then waits for a response.
-# Authors: Owen Bramley 
+# Authors: Owen Bramley, Christian Komo
 
 import serial
 import time
+from const import * # import constants from the const.py file
 
-# open communication to the serial port of the Arduino 
-BAUDRATE = 115200
-PORT = '/dev/cu.usbmodem101'
+BAUDRATE = ARDUINO_BAUDRATE
+PORT = ARDUINO_PORT
+LOGFILE = "logs/datalog"+str(time.time())+".txt"
+
 try:
     serialcomm = serial.Serial(PORT, BAUDRATE)
     serialcomm.timeout = 1
@@ -17,16 +19,23 @@ except:
     print("Could not communicate with arduino. Check the port and baudrate.")
     exit() # exit the program if the serial port cannot be opened
 
-def sendCommand(command = '0'):
+# send command to arduino and request response
+# first parameter: takes command in "<>" form
+# second parameter: log data on/off
+def sendCommand(command = '0', log = False):
     i = command.strip()
     if i == '0':
         return "Invalid command"
     serialcomm.write(i.encode())
     time.sleep(0.5)
     # read the response from the Arduino
-    print(serialcomm.readline().decode('ascii'))
-    serialcomm.close() # close the serial port
-
-# log data recived from the Arduino
-# catch any errors that may occur and add them to the log
+    try:
+        if (log): # log data recived from the Arduino if log is True
+            file = open(LOGFILE, "a")
+            file.write(serialcomm.readline().decode('ascii'))
+        print(serialcomm.readline().decode('ascii'))
+        serialcomm.close() # close the serial port
+    except:
+        file = open(LOGFILE, "a")
+        file.write("Error") # caught errors added to log
 
---
+++ controls/extra/const.py
@@ -0,0 +1,2 @@
+ARDUINO_PORT = '/dev/cu.usbmodem101' #define arduino port
+ARDUINO_BAUDRATE = 115200 #define arduino baudrate
